59e26400a6b614ec2344164220e3d85e8337f4f5
Rodrigo Telles <rtellez700@gmail.com>
2023-08-27 22:02:05 -0700

    Bump version: 0.2.3 → 0.2.4

--- mecode_viewer/__init__.py
+++ mecode_viewer/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Rodrigo Telles"""
 __email__ = 'rtelles@g.harvard.edu'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 from .mecode_viewer import *
--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/rtellez700/mecode_viewer',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 )
