ba79f587800dfaf6d0380c8f6b403bf7db5569f5
Rodrigo Telles <rtellez700@gmail.com>
2023-12-16 15:26:53 -0800

    update deps

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'numpy', 'matplotlib', 'gcode_helpers', 'vpython'
+    'numpy', 'matplotlib', 'gcode_helpers', 'vpython', 'mpl_toolkits'
 ]
 
 test_requirements = [ ]
